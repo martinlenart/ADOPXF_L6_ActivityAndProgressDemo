3c1676bc3232fc7abde5de7acae00138d7b0a747
Martin Lenart <martin_lenart@icloud.com>
2022-01-11 17:20:13 +0100

    Add project files.

---
+++ ActivityAndProgressDemo/ActivityAndProgressDemo/Views/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ActivityAndProgressDemo.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        bool isTaskRunning;
+        float progress = 0f;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            UpdateUiState();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            progress += 0.2f;
+
+            if (progress > 1)
+            {
+                progress = 0;
+            }
+
+            // directly set the new progress value
+            defaultProgressBar.Progress = progress;
+
+            // animate to the new value over 750 milliseconds using Linear easing
+            await styledProgressBar.ProgressTo(progress, 750, Easing.Linear);
+        }
+        void OnButtonClicked1(object sender, EventArgs e)
+        {
+            isTaskRunning = !isTaskRunning;
+            UpdateUiState();
+        }
+
+
+        void UpdateUiState()
+        {
+            runningStatusLabel.Text = isTaskRunning ? "A task is in progress." : "All tasks complete!";
+            defaultActivityIndicator.IsRunning = isTaskRunning;
+            styledActivityIndicator.IsRunning = isTaskRunning;
+        }
+
+    }
+}
